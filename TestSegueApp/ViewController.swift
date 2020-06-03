4397595a5cd1116de801e7d27cf7d9bbcb933312
KryukovAndrey <kryukov.ios@yandex.ru>
2020-06-03 10:21:16 +0300

    Initial Commit

---
+++ TestSegueApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestSegueApp
+//
+//  Created by Andrey on 03.06.2020.
+//  Copyright © 2020 Andrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
